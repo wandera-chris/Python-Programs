e589f650bfc026d4763b8b766151c4c7a93553ff
wandera-chris <christopherwanyama2000@gmail.com>
2022-02-07 13:37:53 +0300

    add the user_verification.py file

---
+++ user_verification.py
@@ -0,0 +1,11 @@
+username = 'chris' 
+password = '123chris@321' 
+userInput = input("What is your username?\ n")
+if userInput == username:
+a=input("Password?\ n") 
+     if a == password:
+         print("Welcome!")
+else: 
+    print("That is the wrong password.")
+else: 
+    print("That is the wrong username.")
